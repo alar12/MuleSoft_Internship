bc8f4b2ddf2d71978911372febdc32bd672007db
alar12 <52037661+alar12@users.noreply.github.com>
2021-11-04 12:37:56 +0530

    Update retrieve_data.java

--- src/mulesoft_sqlite/retrieve_data.java
+++ src/mulesoft_sqlite/retrieve_data.java
@@ -12,7 +12,7 @@ public class retrieve_data {
 		 try {
 			 Class.forName("org.sqlite.JDBC");
 			 String dbURL ="jdbc:sqlite:movies.db";
-			 Connection conn=DriverManager.getConnection(dbURL);
+			 Connection conn=DriverManager.getConnection("jdbc:sqlite:movies.db");
 			 if(conn!=null) {
 				 System.out.println("connection established successful");
 				Statement st=conn.createStatement();				
@@ -24,14 +24,6 @@ public class retrieve_data {
 				 while(rs.next()) {
 					 System.out.println(rs.getString(1)+"\t"+rs.getString(2)+"\t"+rs.getString(3)+"\t"+rs.getInt(4));
 		 }
-				
-				 
-			
-				
-			 }
-			 else {
-				 System.out.println("not connected");
-			 }
 		 }
 		 catch(Exception e) {
 			 System.out.println(e);
